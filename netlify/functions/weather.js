6ecde49b5cc8ad3ae90ffd25e081daeec91a647a
Robert Gorman <rbtgorman@gmail.com>
2025-09-02 13:41:45 -0400

    weather.js

---
+++ netlify/functions/weather.js
@@ -0,0 +1,53 @@
+const fs = require('fs');
+const path = require('path');
+
+exports.handler = async (event, context) => {
+  const { team } = event.queryStringParameters || {};
+  
+  if (!team) {
+    return {
+      statusCode: 400,
+      headers: { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*' },
+      body: JSON.stringify({ 
+        error: 'Team parameter required',
+        usage: 'GET /.netlify/functions/weather?team=BOS'
+      })
+    };
+  }
+  
+  try {
+    const dataPath = path.join(process.cwd(), 'data/park-factors.json');
+    if (fs.existsSync(dataPath)) {
+      const data = JSON.parse(fs.readFileSync(dataPath, 'utf8'));
+      const teamData = data.factors.find(f => f.team === team.toUpperCase());
+      
+      if (teamData) {
+        return {
+          statusCode: 200,
+          headers: { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*' },
+          body: JSON.stringify({
+            team: teamData.team, ballpark: teamData.ballpark,
+            weather: {
+              temperature: teamData.temperatureRange,
+              condition: teamData.weatherCondition,
+              wind: `${teamData.windDirection} ${teamData.windSpeed}mph`
+            }
+          })
+        };
+      }
+    }
+    
+    return {
+      statusCode: 404,
+      headers: { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*' },
+      body: JSON.stringify({ error: 'Team not found' })
+    };
+    
+  } catch (error) {
+    return {
+      statusCode: 500,
+      headers: { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*' },
+      body: JSON.stringify({ error: error.message })
+    };
+  }
+};
